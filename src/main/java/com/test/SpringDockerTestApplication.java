546bc8909b0deb8e2549191f58fc66c92ace48ea
Rajkumar Katanor <rkatanor@valuelabs.com>
2019-04-19 16:05:20 +0530

    inital commit -2

--- src/main/java/com/test/SpringDockerTestApplication.java
+++ src/main/java/com/test/SpringDockerTestApplication.java
@@ -2,7 +2,8 @@ package com.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -12,10 +13,10 @@ public class SpringDockerTestApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDockerTestApplication.class, args);
 	}
-	   @RequestMapping("/")
-       public String home() {
-               return "Hello Docker World";
-       }
+	@GetMapping("/greet/{name}")
+	 public String greeting(@PathVariable String name) {
+	    return "Hi!! " + name;
+	 }
 
 
 }
